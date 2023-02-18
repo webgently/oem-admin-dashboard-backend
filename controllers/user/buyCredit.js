636a0c969b09aba7924c109cfe0670ae24d9d9f0
webgently <oleksandrshkabura811@gmail.com>
2023-02-18 08:41:17 +0300

    update: buy credit function

--- controllers/user/buyCredit.js
+++ controllers/user/buyCredit.js
@@ -60,12 +60,12 @@ const buyCredit = async (req, res, next) => {
       const userMail = `
         <div style="display: flex; justify-content: center">
           <div style="padding: 10vh 14vw;">
-              <h2 style="text-align: center">Receipt from OEMSERVICE</h2>
+              <h2 style="text-align: center">Receipt from OEM Automotive Svenska AB</h2>
               <p style="text-align: center">Receipt #: ${other.receipt}</p>
               <div style="display: flex; padding-top: 2vh">
                   <div style="flex-direction: column; padding: 0 4vw;">
                       <p style="font-weight: bold">AMOUNT PAID</p>
-                      <p>Kr${other.netAmount}</p>
+                      <p>Kr${other.netAmoun.toFixed(2)}</p>
                   </div>
                   <div style="flex-direction: column; padding: 0 4vw">
                       <p style="font-weight: bold">DATE PAID</p>
@@ -80,72 +80,66 @@ const buyCredit = async (req, res, next) => {
               <div style="display: flex; padding: 0 4vw">
                   <ul style="list-style-type: none; font-weight: bold; padding-left: 2vw">
                       <li>Name:</li>
-                      <li>Email:</li>
-                      <li>Contact:</li>
-                      <li>VAT Number:</li>
-                      <li>Account Status:</li>
-                      <li>Region:</li>
-                      <li>Country:</li>
-                      <li>City:</li>
                       <li>Address:</li>
                       <li>Zip Code:</li>
+                      <li>City:</li>
+                      <li>Country:</li>
+                      <li>Telephone:</li>
+                      <li>VAT Number:</li>
                   </ul>
                   <ul style="list-style-type: none;">
                       <li>${account.name}</li>
-                      <li>${token.email}</li>
-                      <li>${account.phone}</li>
-                      <li>${account.vatNumber}</li>
-                      <li>${account.status}</li>
-                      <li>${account.subcontinent}</li>
-                      <li>${account.country}</li>
-                      <li>${account.city}</li>
                       <li>${account.address}</li>
                       <li>${account.zcode}</li>
+                      <li>${account.city}</li>
+                      <li>${account.country}</li>
+                      <li>${account.phone}</li>
+                      <li>${account.vatNumber}</li>
                   </ul>
               </div>
               <h4 style="padding: 0 4vw">SUMMARY</h4>
               <div style="padding: 0 10vw">
                 <p>
-                  <span>Bought ${other.credits} Credits</span>
-                  <span style="float: right">Kr${Number(
+                  <span>${other.credits} Fileservice credit(s)</span>
+                  <span style="float: right">${Number(
                     other.netAmount - other.fee - other.vatCharge
                   )}</span>
                 </p>
                 <div style="border: 1px solid #80808075"></div>
                 <p>
-                  <span>Handling Fee</span>
-                  <span style="float: right">Kr${other.fee}</span>
+                  <span>Handling fee</span>
+                  <span style="float: right">${other.fee}</span>
                 </p>
                 <div style="border: 1px solid #80808075"></div>
                 <p>
-                  <span>${other.tax}% VAT Charges</span>
+                  <span>VAT(${other.tax}%)</span>
                   <span style="float: right">${other.vatCharge}</span>
                 </p>
                 <div style="border: 1px solid #80808075"></div>
                 <p style="font-weight: bold;">
                   <span>Amount charged</span>
-                  <span style="float: right">Kr${other.netAmount}</span>
+                  <span style="float: right">${other.netAmount} SEK</span>
                 </p>
               </div>
               <div style="border-top: 1px solid gray; border-bottom: 1px solid gray; padding: 2vh 0; margin: 4vh 4vw">
               <div style="text-align: center">
-                If you have any questions, contact us at&nbsp;<b>${
+                If you have any questions, contact us at&nbsp;<span style="text-decoration: underline; color: blue;"><b>${
                   process.env.SUPPORT_EMAIL
-                }</b>.
+                }</b></span>.
               </div>
             </div>
-            <p style="text-align: center;">You're receiving this email because you made a purchase at OEMSERVICE</p>
+            <p style="text-align: center;">OEM Automotive Svenska AB – Org: 559417-9839 – VAT: SE559417983901</p>
           </div>
         </div>`;
       const adminMail = `
         <div style="display: flex; justify-content: center">
           <div style="padding: 10vh 14vw;">
-              <h2 style="text-align: center">Receipt from OEMSERVICE</h2>
+              <h2 style="text-align: center">Receipt from OEM Automotive Svenska AB</h2>
               <p style="text-align: center">Receipt #: ${other.receipt}</p>
               <div style="display: flex; padding-top: 2vh">
                   <div style="flex-direction: column; padding: 0 4vw;">
                       <p style="font-weight: bold">AMOUNT PAID</p>
-                      <p>Kr${other.netAmount}</p>
+                      <p>Kr${other.netAmoun.toFixed(2)}</p>
                   </div>
                   <div style="flex-direction: column; padding: 0 4vw">
                       <p style="font-weight: bold">DATE PAID</p>
@@ -160,62 +154,55 @@ const buyCredit = async (req, res, next) => {
               <div style="display: flex; padding: 0 4vw">
                   <ul style="list-style-type: none; font-weight: bold; padding-left: 2vw">
                       <li>Name:</li>
-                      <li>Email:</li>
-                      <li>Contact:</li>
-                      <li>VAT Number:</li>
-                      <li>Account Status:</li>
-                      <li>Region:</li>
-                      <li>Country:</li>
-                      <li>City:</li>
                       <li>Address:</li>
                       <li>Zip Code:</li>
+                      <li>City:</li>
+                      <li>Country:</li>
+                      <li>Telephone:</li>
+                      <li>VAT Number:</li>
                   </ul>
                   <ul style="list-style-type: none;">
                       <li>${account.name}</li>
-                      <li>${token.email}</li>
-                      <li>${account.phone}</li>
-                      <li>${account.vatNumber}</li>
-                      <li>${account.status}</li>
-                      <li>${account.subcontinent}</li>
-                      <li>${account.country}</li>
-                      <li>${account.city}</li>
                       <li>${account.address}</li>
                       <li>${account.zcode}</li>
+                      <li>${account.city}</li>
+                      <li>${account.country}</li>
+                      <li>${account.phone}</li>
+                      <li>${account.vatNumber}</li>
                   </ul>
               </div>
               <h4 style="padding: 0 4vw">SUMMARY</h4>
               <div style="padding: 0 10vw">
                 <p>
-                  <span>Bought ${other.credits} Credits</span>
-                  <span style="float: right">Kr${Number(
+                  <span>${other.credits} Fileservice credit(s)</span>
+                  <span style="float: right">${Number(
                     other.netAmount - other.fee - other.vatCharge
                   )}</span>
                 </p>
+                <div style="border: 1px solid #80808075"></div>
+                <p>
+                  <span>Handling fee</span>
+                  <span style="float: right">${other.fee}</span>
                 </p>
                 <div style="border: 1px solid #80808075"></div>
                 <p>
-                  <span>Handling Fee</span>
-                  <span style="float: right">Kr${other.fee}</span>
-                </p>
-                <div style="border: 1px solid #80808075"></div>
-                <p>
-                  <span>${other.tax}% VAT Charges</span>
+                  <span>VAT(${other.tax}%)</span>
                   <span style="float: right">${other.vatCharge}</span>
                 </p>
                 <div style="border: 1px solid #80808075"></div>
                 <p style="font-weight: bold;">
                   <span>Amount charged</span>
-                  <span style="float: right">Kr${other.netAmount}</span>
+                  <span style="float: right">${other.netAmount} SEK</span>
                 </p>
               </div>
               <div style="border-top: 1px solid gray; border-bottom: 1px solid gray; padding: 2vh 0; margin: 4vh 4vw">
               <div style="text-align: center">
-                If you have any questions, contact us at&nbsp;<b>${
+                If you have any questions, contact us at&nbsp;<span style="text-decoration: underline; color: blue;"><b>${
                   process.env.SUPPORT_EMAIL
-                }</b>.
+                }</b></span>.
               </div>
             </div>
-            <p style="text-align: center;">You're receiving this email because you made a purchase at OEMSERVICE</p>
+            <p style="text-align: center;">OEM Automotive Svenska AB – Org: 559417-9839 – VAT: SE559417983901</p>
           </div>
         </div>`;
       
